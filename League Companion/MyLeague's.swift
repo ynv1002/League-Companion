54be16ff514e605f4cdad2e6cf6da9b0894f429a
Marty Estrin <122707092+martyestrin@users.noreply.github.com>
2023-06-08 21:04:10 -0700

    Update MyLeague's.swift

--- League Companion/MyLeague's.swift
+++ League Companion/MyLeague's.swift
@@ -8,3 +8,14 @@
 import Foundation
 
 print("Your Fantasy Football Leauges:" \n)
+
+Button() {
+  Text("Add New Leauge")
+  func addLeauge(name: string){
+    var rosterPositions: [String: Int] = ["QB": 0, "RB": 0, "WR": 0, "TE": 0, "FLX": 0, "DST": 0, "K": 0, "Bench": 0]
+    for position in rosterPositions {
+      Text("Enter quantity for ", position, "position")
+      rosterPositions[position] = readLine()   
+    }
+  }
+}
