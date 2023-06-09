3b8a418f95e630cb8d20639d6f7c9d5ba72db474
ynv1002 <134446873+ynv1002@users.noreply.github.com>
2023-06-08 21:13:54 -0700

    Update ContentView.swift

--- League Companion/ContentView.swift
+++ League Companion/ContentView.swift
@@ -8,30 +8,72 @@
 import SwiftUI
 import FirebaseAuth
 
-
 struct ContentView: View {
     @State private var username: String = ""
     @State private var password: String = ""
+    @State private var showMyLeagues = false // Add a state variable for navigation
 
     var body: some View {
-        VStack {
-            TextField("Username", text: $username)
-            SecureField("Password", text: $password)
-            Button(action: {
-                // Handle sign up
-                Auth.auth().createUser(withEmail: username, password: password) { (authResult, error) in
-                    if let error = error {
-                        // An error occurred while creating the user
-                        print("Error occurred: \(error.localizedDescription)")
-                    } else {
-                        // User was created successfully, now you can navigate them into the app, or do other things like sending verification email etc.
-                        print("User created successfully!")
-                    }
+        NavigationView {
+            VStack(spacing: 20) {
+                Image("logo") // Add your app logo image here
+                    .resizable()
+                    .frame(width: 100, height: 100)
+                
+                VStack(spacing: 16) {
+                    TextField("Username", text: $username)
+                        .padding()
+                        .background(Color(.secondarySystemBackground))
+                        .cornerRadius(8)
+                    
+                    SecureField("Password", text: $password)
+                        .padding()
+                        .background(Color(.secondarySystemBackground))
+                        .cornerRadius(8)
                 }
-            }) {
-                Text("Sign Up")
+                
+                Button(action: {
+                    // Handle sign up
+                    Auth.auth().createUser(withEmail: username, password: password) { (authResult, error) in
+                        if let error = error {
+                            // An error occurred while creating the user
+                            print("Error occurred: \(error.localizedDescription)")
+                        } else {
+                            // User was created successfully
+                            print("User created successfully!")
+                            showMyLeagues = true // Set the state variable to trigger navigation
+                        }
+                    }
+                }) {
+                    Text("Sign Up")
+                        .font(.headline)
+                        .foregroundColor(.white)
+                        .padding()
+                        .frame(maxWidth: .infinity)
+                        .background(Color.blue)
+                        .cornerRadius(8)
+                }
             }
+            .padding()
+            .navigationBarTitle("League Companion") // Set the navigation bar title
+            .background(
+                NavigationLink(destination: MyLeaguesView(), isActive: $showMyLeagues) {
+                    EmptyView()
+                }
+            )
         }
-        .padding()
+    }
+}
+
+struct MyLeaguesView: View {
+    var body: some View {
+        Text("My Leagues")
+            .font(.largeTitle)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
     }
 }
