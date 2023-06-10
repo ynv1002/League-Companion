f3ecf393163bf5c6650a31b0eb0b41e89390647b
ynv1002 <134446873+ynv1002@users.noreply.github.com>
2023-06-09 17:32:06 -0700

    Update MyLeague's.swift

--- League Companion/MyLeague's.swift
+++ League Companion/MyLeague's.swift
@@ -5,17 +5,24 @@
 //  Created by Yaniv Naggar on 6/8/23.
 //
 
-import Foundation
+import SwiftUI
 
-print("Your Fantasy Football Leauges:" \n)
-
-Button() {
-  Text("Add New Leauge")
-  func addLeauge(name: string){
-    var rosterPositions: [String: Int] = ["QB": 0, "RB": 0, "WR": 0, "TE": 0, "FLX": 0, "DST": 0, "K": 0, "Bench": 0]
-    for position in rosterPositions {
-      Text("Enter quantity for ", position, "position")
-      rosterPositions[position] = readLine()   
+struct MyLeagueView: View {
+    var body: some View {
+        NavigationView {
+            VStack {
+                NavigationLink(destination: LoginView()) {
+                    Text("Log In")
+                }
+            }
+            
+        }
     }
-  }
+    
+    
+    struct MyLeagueView_Previews: PreviewProvider {
+        static var previews: some View {
+            MyLeagueView()
+        }
+    }
 }
