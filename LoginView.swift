dc569bcf6b03343162f5a52ab037c99136b267a5
ynv1002 <134446873+ynv1002@users.noreply.github.com>
2023-06-16 13:11:02 -0700

    Update LoginView.swift

--- LoginView.swift
+++ LoginView.swift
@@ -8,50 +8,68 @@
 import SwiftUI
 import Firebase
 
-func login(email: String, password: String) {
-    Auth.auth().signIn(withEmail: email, password: password) { authResult, error in
-        if let error = error {
-            print("An error occurred: \(error.localizedDescription)")
-        } else {
-            print("User logged in successfully")
-            MyLeagueView()
-            // Navigate to your main app view
-        }
-    }
-}
 struct LoginView: View {
     @State private var email = ""
     @State private var password = ""
+    @State private var isLoggedIn = false  // Add a state variable for navigation
 
     var body: some View {
+        ZStack {
+            // Add a background gradient
+            LinearGradient(gradient: Gradient(colors: [.blue, .purple]), startPoint: .topLeading, endPoint: .bottomTrailing)
+                .ignoresSafeArea()
+
+            if isLoggedIn {
+                MyLeagueView() // This view should be navigated when login is successful
+            } else {
+                loginContent
+            }
+        }
+    }
+    
+    var loginContent: some View {
         VStack {
             Text("Login")
                 .font(.largeTitle)
-
+                .fontWeight(.bold)
+                .foregroundColor(.white)
+            
             TextField("Email", text: $email)
-                .padding()
+                .textFieldStyle()
                 .autocapitalization(.none)
-
+            
             SecureField("Password", text: $password)
-                .padding()
+                .textFieldStyle()
                 .autocapitalization(.none)
-
+            
             Button(action: {
                 login(email: email, password: password)
             }) {
                 Text("Login")
-                    .padding()
-                    .background(Color.blue)
-                    .foregroundColor(.white)
-                    .cornerRadius(10)
+                    .buttonStyle()
             }
-        }.padding()
+        }
+        .padding(.horizontal, 24)
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color.black.opacity(0.3))
+        .cornerRadius(20)
+        .shadow(color: .black, radius: 10, x: 0, y: 10)
     }
-    struct LoginView_Previews: PreviewProvider {
-        static var previews: some View {
-            LoginView()
+    
+    func login(email: String, password: String) {
+        Auth.auth().signIn(withEmail: email, password: password) { authResult, error in
+            if let error = error {
+                print("An error occurred: \(error.localizedDescription)")
+            } else {
+                print("User logged in successfully")
+                self.isLoggedIn = true // Set the state variable to trigger navigation
+            }
         }
     }
-
 }
 
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
